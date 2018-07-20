4fc2d530fec95ab1a2adc5ee5a375b5ee48e06a8
Michael van der Weg <michael@joinbox.com>
2018-07-20 17:26:53 +0200

    chore: add main for integration

---
+++ index.integration.mjs
@@ -0,0 +1,25 @@
+'use strict';
+
+
+
+import log from 'ee-log';
+import {ServiceManager} from 'rda-service';
+
+
+(async () => {
+
+    const serviceManager = new ServiceManager({
+        args: '--related-errors --data-for-beta --integration --log-level=error+ --log-module=*'.split(' ')
+    });
+
+    await serviceManager.startServices('rda-service-registry');
+    await serviceManager.startServices('rda-compute', 'rda-compute', 'rda-compute', 'rda-compute', 'rda-compute', 'rda-compute', 'rda-compute', 'rda-compute');
+    await serviceManager.startServices('rda-compute', 'rda-compute', 'rda-compute', 'rda-compute', 'rda-compute', 'rda-compute', 'rda-compute', 'rda-compute');
+    await serviceManager.startServices('rda-cluster');
+    await serviceManager.startServices('rda-coordinator');
+    await serviceManager.startServices('infect-rda-sample-storage');
+    await serviceManager.startServices('api');
+    await serviceManager.startServices('infect-rda-sample-importer');
+})().then(() => {
+    log.success('application is ready');
+}).catch(log);
