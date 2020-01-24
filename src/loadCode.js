c82ed1eccc1ba5a6ced76c33e29653aea3add093
Michael van der Weg <michael@joinbox.com>
2020-01-24 18:44:19 +0100

    feat: add code loader

---
+++ src/loadCode.js
@@ -0,0 +1,54 @@
+import HTTP2Client from '@distributed-systems/http2-client';
+import log from 'ee-log';
+import RegistryClient from '@infect/rda-service-registry-client';
+
+
+
+
+class CodeLoader {
+
+    constructor(registryHost = 'http://l.dns.porn:9000') {
+        this.client = new RegistryClient(registryHost);
+        this.httpClient = new HTTP2Client();
+    }
+
+
+    /**
+     * shut the class down
+     */
+    async end() {
+        await this.httpClient.end();
+    }
+
+
+
+    async load() {
+        const storageHost = await this.client.resolve('infect-rda-sample-storage');
+
+        log.info('loading source code ....');
+        await this.httpClient.post(`${storageHost}/infect-rda-sample-storage.source-code-loader`)
+            .expect(201)
+            .send();
+
+
+        log.info('linking source code ....');
+        await this.httpClient.patch(`${storageHost}/infect-rda-sample-storage.source-code-loader/infect-human`)
+            .expect(200)
+            .send();
+    }
+}
+
+
+
+
+const loader = new CodeLoader();
+
+
+
+loader.load().then(() => {
+    log.success('the source code was loaded and linked');
+    loader.end();
+}).catch((err) => {
+    log.error(err);
+    loader.end();
+});
