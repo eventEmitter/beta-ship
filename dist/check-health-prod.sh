eb75d2426ad7cf5d394d594343ae689c7057a038
Lina <lina@vanderweg.ch>
2021-04-29 10:11:18 +0200

    chore: add health checkers

---
+++ dist/check-health-prod.sh
@@ -0,0 +1,22 @@
+#!/bin/bash              
+URL=https://api.infect.info/rda/v2/rda.data
+STATUS=$(curl -s -o /dev/null -w "%{http_code}\n" -m 30 $URL)
+ScriptLoc=$(readlink -f "$0")
+
+if [ $STATUS == 200 ] ; then
+  echo "$URL is up, returned $STATUS"
+else                     
+  echo "$URL is not up, returned $STATUS"
+
+  systemctl restart rda
+  sleep 60
+
+  cd /home/ubuntu/apps/beta-ship && /usr/bin/node --max-old-space-size=16000 --experimental-modules --no-warnings --experimental-vm-modules src/createCluster.js --prod --vet --log-level=error+ --log-module=* 2>&1 | /usr/bin/logger -t RDA
+  sleep 60
+
+  cd /home/ubuntu/apps/beta-ship && /usr/bin/node --max-old-space-size=16000 --experimental-modules --no-warnings --experimental-vm-modules src/createCluster.js --prod --log-level=error+ --log-module=* 2>&1 | /usr/bin/logger -t RDA
+  sleep 600
+fi
+
+sleep 10
+exec "$ScriptLoc"
